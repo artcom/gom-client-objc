ad4cb26d8ff92c8ee26fb5b3aa2c6a8567ebccb3
Julian Krumow <julian.krumow@artcom.de>
2014-04-14 22:30:26 +0200

    Added test for GNP after deletion.

--- gom-client-demo_iOS/gom-client-demo_iOSTests/GomClientTest.m
+++ gom-client-demo_iOS/gom-client-demo_iOSTests/GomClientTest.m
@@ -377,4 +377,62 @@ float const TIMEOUT = 10.0;
     XCTAssertTrue([[payload valueForKeyPath:@"attribute.value"] isEqualToString:ATTRIBUTE_2_1_VALUE_NEW], @"attribute.value should be equal to the reference value.");
 }
 
+- (void)testReceiveGNPAfterDestroy
+{
+    [self prepare];
+    
+    static NSDictionary *_initialResponse = nil;
+    static NSDictionary *_destroyResponse = nil;
+    
+    [_gomClient updateAttribute:ATTRIBUTE_2_1_PATH withValue:ATTRIBUTE_2_1_VALUE completionBlock:^(NSDictionary *response, NSError *error) {
+        
+        if (response) {
+            
+            [_gomClient registerGOMObserverForPath:ATTRIBUTE_2_1_PATH options:nil clientCallback:^(NSDictionary *response) {
+                
+                if ([response[@"event_type"] isEqualToString:@"initial"]) {
+                    _initialResponse = response;
+                    
+                    // trigger GNP by updating attribute
+                    [_gomClient destroy:ATTRIBUTE_2_1_PATH completionBlock:nil];
+                    
+                } else if ([response[@"event_type"] isEqualToString:@"delete"]) {
+                    _destroyResponse = response;
+                    
+                    // test case officially finished
+                    [self notify:kXCTUnitWaitStatusSuccess];
+                }
+                
+            }];
+            
+        }
+    }];
+    
+    [self waitForStatus:kXCTUnitWaitStatusSuccess timeout:TIMEOUT];
+    
+    // check initial response
+    XCTAssertTrue([_initialResponse[@"path"] isEqualToString:ATTRIBUTE_2_1_PATH], @"path should be equal to the reference value.");
+    XCTAssertTrue([_initialResponse[@"event_type"] isEqualToString:@"initial"], @"event_type should be equal to the reference value.");
+    
+    NSDictionary *payload = _initialResponse[@"payload"];
+    XCTAssertNotNil(payload[@"attribute"], @"Attribute entry must not be nil.");
+    XCTAssertNotNil([payload valueForKeyPath:@"attribute.ctime"], @"attribute.ctime should not be nil.");
+    XCTAssertNotNil([payload valueForKeyPath:@"attribute.mtime"], @"attribute.mtime should not be nil.");
+    XCTAssertTrue([[payload valueForKeyPath:@"attribute.node"] isEqualToString:NODE_2_PATH], @"node.uri should be equal to the reference value.");
+    XCTAssertTrue([[payload valueForKeyPath:@"attribute.type"] isEqualToString:ATTRIBUTE_2_1_TYPE], @"attribute.type should be equal to the reference value.");
+    XCTAssertTrue([[payload valueForKeyPath:@"attribute.name"] isEqualToString:ATTRIBUTE_2_1_NAME], @"attribute.name should be equal to the reference value.");
+    XCTAssertTrue([[payload valueForKeyPath:@"attribute.value"] isEqualToString:ATTRIBUTE_2_1_VALUE], @"attribute.value should be equal to the reference value.");
+    
+    // check response from update
+    XCTAssertTrue([_destroyResponse[@"path"] isEqualToString:ATTRIBUTE_2_1_PATH], @"path should be equal to the reference value.");
+    XCTAssertTrue([_destroyResponse[@"event_type"] isEqualToString:@"delete"], @"event_type should be equal to the reference value.");
+    
+    payload = _destroyResponse[@"payload"];
+    XCTAssertNotNil(payload[@"attribute"], @"Attribute entry must not be nil.");
+    XCTAssertTrue([[payload valueForKeyPath:@"attribute.node"] isEqualToString:NODE_2_PATH], @"node.uri should be equal to the reference value.");
+    XCTAssertTrue([[payload valueForKeyPath:@"attribute.type"] isEqualToString:ATTRIBUTE_2_1_TYPE], @"attribute.type should be equal to the reference value.");
+    XCTAssertTrue([[payload valueForKeyPath:@"attribute.name"] isEqualToString:ATTRIBUTE_2_1_NAME], @"attribute.name should be equal to the reference value.");
+    XCTAssertTrue([[payload valueForKeyPath:@"attribute.value"] isKindOfClass:[NSNull class]], @"attribute.value should be of class NSNull");
+}
+
 @end
